84d4ebeaf6b6ef1edd00d8d7dd7e037d523a89d2
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-12-17 00:26:16 +0100

    fetching and showing data

---
+++ zonky-challenge/src/components/FlightOverview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Flights</h1>
+        <b-table striped hover :items="flights" :fields="['cityFrom', 'cityTo', 'price', ]">
+            <template v-slot:cell(name)="data">
+             <!-- <router-link :to="`/pet/${species}/${data.index}`">{{ data.value }}</router-link> -->
+            </template>
+        </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        flights: Object
+    }
+}
+</script>
